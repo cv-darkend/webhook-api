5352365cf00b95fd350b8f13a3c6ee6c1e1f3afc
Alejandro Diaz <58312576+cv-darkend@users.noreply.github.com>
2025-04-07 20:46:33 -0500

    Add root_path

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Query, Path, Request, Header
 from typing import List, Annotated
 
 
-app = FastAPI()
+app = FastAPI(root_path='/api')
 
 @app.get("/")
 async def read_root():
